8c29e110f16bcec12987280e71fca0faeed3e0b6
Denes0216 <orban.denes1199@gmail.com>
2025-11-23 19:33:04 +0100

    Add animated background to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
-export default function Hero() {
+import AnimatedBackground from "./AnimatedBackground";
 
+export default function Hero() {
   const goToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -8,20 +9,23 @@ export default function Hero() {
   };
 
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen bg-gray-50 m-0 text-center p-4 gap-10">
-      <h1 className="text-4xl font-bold mb-4 animate-[appearFromLeft_1.5s_ease-out_forwards]">
+    <section className="relative flex flex-col items-center justify-center min-h-screen bg-linear-to-br from-gray-50 to-gray-100 m-0 text-center p-4 gap-10 overflow-hidden">
+      <AnimatedBackground />
+      <h1 className="relative z-10 text-4xl font-bold mb-4 animate-[appearFromLeft_1.5s_ease-out_forwards]">
         Welcome to My Portfolio
       </h1>
-      <p className="text-lg animate-[appearFromBottom_1.5s_ease-out_forwards] text-gray-400">
+      <p className="relative z-10 text-lg animate-[appearFromBottom_1.5s_ease-out_forwards] text-gray-400">
         Hi! I'm Denes Orban, a passionate developer specializing in building
         amazing web applications.
       </p>
-      <div className="flex space-x-4 mt-6">
+      <div className="relative z-10 flex space-x-4 mt-6">
         <button className="mt-6 px-6 bg-gray-500 border border-gray-700 text-white py-2 rounded-3xl hover:bg-gray-700">
           Get in Touch
         </button>
-        <button className="mt-6 px-6 bg-gray-50 border border-gray-300 text-gray-700 py-2 rounded-3xl hover:bg-gray-200 "
-        onClick={goToSection.bind(null, "projects")}>
+        <button
+          className="mt-6 px-6 bg-gray-50 border border-gray-300 text-gray-700 py-2 rounded-3xl hover:bg-gray-200 "
+          onClick={goToSection.bind(null, "projects")}
+        >
           View My Work
         </button>
       </div>
