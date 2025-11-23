47848bba7e4fbe433b0d99d5d6e336268dcff32b
Denes0216 <orban.denes1199@gmail.com>
2025-11-23 19:33:56 +0100

    Remove cursor orb

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import About from "./components/About";
 import BackToTop from "./components/BackToTop";
-import CursorOrb from "./components/CursorOrb";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
-      <CursorOrb />
       <Hero />
       <About />
       <Projects />
